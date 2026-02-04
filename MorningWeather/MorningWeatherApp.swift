62898563d5af9ab66b5b0ca11c512992091e2719
Arian <arian@Arians-MacBook-Pro.local>
2026-02-04 22:08:02 +0100

    Initial Xcode project setup

---
+++ MorningWeather/MorningWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningWeatherApp.swift
+//  MorningWeather
+//
+//  Created by Arian on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MorningWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorningWeatherTests/MorningWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorningWeatherTests.swift
+//  MorningWeatherTests
+//
+//  Created by Arian on 2/4/26.
+//
+
+import Testing
+@testable import MorningWeather
+
+struct MorningWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
